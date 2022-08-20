f7f18ef0c5c1a823895b9bfe9e2b6926efe5959d
Aliyu <biggy0023@gmail.com>
2022-08-20 18:21:00 +0100

    created the first version of play method to ensure there's an input

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -2,22 +2,22 @@
 
 require 'pry-byebug'
 
-# creating board
+# create board
+
 board = ['', '', '', '', '', '', '', '', '']
-index = gets.chomp.to_i
 
-def board_layout(board)
-  puts "  #{board[0]} | #{board[1]} | #{board[2]} "
-  puts '-----------'
-  puts "  #{board[3]} | #{board[4]} | #{board[5]} "
-  puts '-----------'
-  puts "  #{board[6]} | #{board[7]} | #{board[8]} "
+def display_board(board)
+  puts "  #{board[0]} | #{board[1]} | #{board[2]}"
+  puts '----------'
+  puts "  #{board[3]} | #{board[4]} | #{board[5]}"
+  puts '----------'
+  puts "  #{board[6]} | #{board[7]} | #{board[8]}"
 end
 
-def move(board, index, player = 'X')
-  puts 'Choose your spot!'
-  board[index] = player
-  board_layout(board)
+def play(board)
+  puts 'Make  a move!'
+  move = gets.chomp.to_i
+  board[move] = 'X'
+  display_board(board)
 end
-
-move(board, index)
+play(board)
