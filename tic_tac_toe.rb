ba064767514120eb7ddc7ebb50bcd57fc1ca8a4a
Aliyu <biggy0023@gmail.com>
2022-09-19 14:29:55 +0100

    Revisiting and doing everything all over again. I am taking the process one method at a time with a clearer head after some intensive study. Currently fiddling around to understand how each component of the game works before i proceed to their inerractions

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,87 +1,89 @@
 # frozen_string_literal: true
 
-# trying to make sense out of this
+# create a board
+# create a player
 
-# thanks TOP :')
+# board and it's layout
+board = ['', '', '', '', '', '', '', '', '']
 
-# below is the winning combination for the game
-WIN_COMBO = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
-
-# the palyer class encompassing everything
-
-@board = ['', '', '', '', '', '', '', '', '']
-# @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-# the layout
-def board_layout
-  puts "  #{@board[0]}  |  #{@board[1]}  |  #{@board[2]}  "
-  puts '---------------'
-  puts "  #{@board[3]}  |  #{@board[4]}  |  #{@board[5]}  "
-  puts '---------------'
-  puts "  #{@board[6]}  |  #{@board[7]}  |  #{@board[8]}  "
+def board_layout(board)
+  puts "  #{board[0]}  |  #{board[1]}  |  #{board[2]}  "
+  puts '- - -+- - -+- - -'
+  puts "  #{board[3]}  |  #{board[4]}  |  #{board[5]}  "
+  puts '- - -+- - -+- - -'
+  puts "  #{board[6]}  |  #{board[7]}  |  #{board[8]}  "
 end
 
-# making 1 to 1 input instead of starting the board count from zero
-def better_input
-  gets.chomp.to_i - 1
-end
+# player class
+class Player
+  attr_accessor :name
 
-# find free positions
-
-def taken?(index)
-  if @board[index] == '' || @board[index] == ' ' || @board[index] == nil
-    return false
-  else
-    return true
+  def initialize(name)
+    @name = name
   end
 end
 
-
-# check if the board is full
-def full?
-  if @board.include?('') || @board.include?(' ') || @board.include?(nil)
-    return false
-  else
-    return true
-  end
+def player_X
+  Player.new('X')
 end
 
-# player1 move
-def player1
-  puts 'Check if spot is taken'
-  if taken?(better_input)
-    puts 'Spot taken'
-  else
-    puts 'spot not taken, make a move'
-    @board[better_input] = 'X'
-    board_layout
-  end
+def player_O
+  Player.new('O')
 end
 
-# player2 move
-def player2
-  puts 'Check if spot is taken'
-  if taken?(better_input)
-    puts 'Spot taken'
-  else
-    puts 'spot not taken, make a move'
-    @board[better_input] = 'O'
-    board_layout
-  end
-end
+=begin
+def choose_player
+  puts 'player X or O?:'
+  player = ''
+  valid_char = false
 
-def player
-  loop do
-    if full?
-      puts 'No more move!'
-      break
-    end
-    if player1
-      player2
-    elsif player2
-      player1
+  while valid_char == false
+    player = gets.chomp.upcase
+
+    if player == 'X'
+      player1 = Player.new('X')
+      puts "Player one marker #{player1.name} confirmed"
+      player2 = Player.new('O')
+      puts "Player two marker #{player2.name} confirmed"
+      puts "Let's begin"
+      valid_char = true
+    elsif player == 'O'
+      player1 = Player.new('O')
+      puts "Player one marker #{player1.name} confirmed"
+      player2 = Player.new('X')
+      puts "Player two marker #{player2.name} confirmed"
+      puts "Let's begin"
+      valid_char = true
+    else
+      puts "#{player} is not a valid input"
+      puts 'Gotta be X or O'
+      valid_char = false
     end
   end
 end
+=end
 
-puts player
+# play game
+player1 = 'X'
+player2 = 'O'
+
+def turn(player1, player2)
+  if player1
+    player2
+  elsif player2
+    player1
+  end
+end
+
+def play(board, player1, player2)
+  moves = 9
+
+  while moves.positive?
+    board_layout(board)
+    board[gets.chomp.to_i] = turn(player1, player2)
+    board_layout(board)
+    moves -= 1
+  end
+end
+
+play(board, player1, player2)
